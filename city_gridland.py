37afa5a12545cf2dad78094b76fbc8dd6e34cf09
sharif <shahanur700@gmail.com>
2024-09-09 23:19:02 +0800

    first

---
+++ city_gridland.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridlandMetro' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#  3. INTEGER k
+#  4. 2D_INTEGER_ARRAY track
+#
+
+def gridlandMetro(n, m, k, track):
+    # Write your code here
+    print(n, m, k, track)
+    pass
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    track = []
+
+    for _ in range(k):
+        track.append(list(map(int, input().rstrip().split())))
+
+    gridlandMetro(n, m, k, track)
+
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
---
+++ timeConverter.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    hour = s.split(':')[0]
+    minute = s.split(':')[1]
+    second = s.split(':')[2]
+    second_value_only = second[:2]
+
+    meridiem = second[2:]
+
+    hour_str = hour
+    if meridiem == 'PM' and hour != '12':
+        hour_str = int(hour) + 12
+
+    if meridiem == 'AM' and hour == '12':
+        hour_str = int(hour) - 12
+
+    # print(f"{hour_str}:{minute}:{second_value_only}")
+
+    return f"{hour_str}:{minute}:{second_value_only}"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
