e4b4e29c0d9ea8c6dcb2ade4c43512d078ec4e7a
sharif <shahanur700@gmail.com>
2024-10-07 14:57:46 +0800

    first

--- Data Structures and Algorithms Specialization/APlusB.py
+++ Data Structures and Algorithms Specialization/APlusB.py
@@ -1,6 +1,15 @@
-def test_sum(first_digit, second_digit):
-    return first_digit + second_digit
+import random
+import sys
 
-if __name__ == '__main__':
-    a, b = map(float, input().split())
-    print(test_sum(a, b))
+n = int(sys.argv [1])
+myseed = int(sys .argv [2])
+random.seed(myseed)
+
+
+
+# def test_sum(first_digit, second_digit):
+#     return first_digit + second_digit
+#
+# if __name__ == '__main__':
+#     a, b = map(float, input().split())
+#     print(test_sum(a, b))
