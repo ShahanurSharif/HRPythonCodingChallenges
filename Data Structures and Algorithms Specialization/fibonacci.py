f3fd1475ba5dc0b82f05371870d890f948cd2659
sharif <shahanur700@gmail.com>
2024-10-09 15:19:13 +0800

    first

--- Data Structures and Algorithms Specialization/fibonacci.py
+++ Data Structures and Algorithms Specialization/fibonacci.py
@@ -1,3 +1,9 @@
+def findFibonacci(n):
+    fib = [0, 1]
+    for i in range(2, n+1):
+        fib.append(fib[i-1] + fib[i-2])
+
+    return fib[n]
 
 if __name__ == "__main__":
-    findFibonacci(100)
+    print(findFibonacci(5))
