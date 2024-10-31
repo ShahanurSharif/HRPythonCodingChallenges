d975efa2c9c10123843745cbbf91a75c5d7ba5c7
sharif <shahanur700@gmail.com>
2024-10-31 11:19:36 +0800

    magic

--- magicSquare.py
+++ magicSquare.py
@@ -17,15 +17,41 @@ import sys
 def formingMagicSquare(s):
     # Write your code here
     sum_of_row = []
+    sum_of_column = []
     diagonal_left_to_right = 0
     diagonal_right_to_left = 0
-    for index in range(len(s)):
-        sum_of_row.append(sum(s[index]))
-        for row_index in range(len(s[index])):
-            if index == row_index:
-                diagonal_left_to_right += s[index][row_index]
 
-    print(sum_of_row, diagonal_left_to_right)
+    for index in range(len(s)):
+        diagonal_left_to_right += s[index][index]
+        diagonal_right_to_left += s[index][len(s) - index - 1]
+        sum_of_row.append(sum(s[index]))
+
+    n = len(s[0])  # Number of columns
+    for col in range(n):
+        sum_of_col = sum(s[row][col] for row in range(len(s)))  # Sum each column
+        sum_of_column.append(sum_of_col)
+
+    all_sums = [diagonal_left_to_right, diagonal_right_to_left, sum_of_column, sum_of_row]
+
+    flat_array = [item for sublist in all_sums for item in (sublist if isinstance(sublist, list) else [sublist])]
+
+    grouping = {}
+    for number in flat_array:
+        if grouping.get(number):
+            grouping[number] += 1
+        else:
+            grouping[number] = 1
+
+    # grouping = dict(sorted(grouping.items(), key=lambda item:item[1], reverse=True))
+    max_value = max(grouping.values())
+    if max_value > 1:
+        desire_number = max(grouping, key=grouping.get)
+    else:
+        desire_number = min(grouping)
+
+    # if it's high, then operation should be addition
+    # if it's low, then operation should be subtraction
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
