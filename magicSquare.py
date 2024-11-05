30d2ee52ff01fa0dfa64c9ec0b3d642c64ea6af5
sharif <shahanur700@gmail.com>
2024-11-05 16:29:29 +0800

    magic

--- magicSquare.py
+++ magicSquare.py
@@ -14,11 +14,24 @@ import sys
 # The function accepts 2D_INTEGER_ARRAY s as parameter.
 #
 def magicNumber(length=0):
-    total = length*(length*length + 1)/2
+    total = length * (length * length + 1) / 2
     return int(total)
 
+
 def is_magic_square(s):
-    pass
+    lrd = 0
+    rld = 0
+    sr = []
+    sc = []
+
+    row_total = len(s[0])
+    for i in range(len(s)):
+        lrd += s[i][i]
+        rld += s[i][row_total - i - 1]
+        sr.append(sum(s[i]))
+        sc.append(sum(s[i][j] for j in range(len(s[i]))))
+
+
 
 def formingMagicSquare(s):
     length = len(s[0])
@@ -29,13 +42,10 @@ def formingMagicSquare(s):
     pass
 
 
-
-
-
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    s= [[4, 9, 2], [3, 5, 7], [8, 1, 5]]
+    s = [[4, 9, 2], [3, 5, 7], [8, 1, 5]]
 
     result = formingMagicSquare(s)
 
