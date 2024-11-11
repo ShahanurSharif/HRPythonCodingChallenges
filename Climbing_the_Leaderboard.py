d0adef46551989cb2e665a23f7934389dc723456
sharif <shahanur700@gmail.com>
2024-11-11 09:19:20 +0800

    magic

---
+++ Climbing_the_Leaderboard.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+def climbingLeaderboard(ranked, player):
+    # Write your code here
+    print(ranked, player)
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    # ranked_count = int(input().strip())
+
+    # ranked = list(map(int, input().rstrip().split()))
+
+    # player_count = int(input().strip())
+
+    # player = list(map(int, input().rstrip().split()))
+
+    ranked = [100, 100, 50, 40, 40, 20, 10]
+    player = [5, 25, 50, 120]
+    result = climbingLeaderboard(ranked, player)
+
+    # fptr.write('\n'.join(map(str, result)))
+    # fptr.write('\n')
+    #
+    # fptr.close()
