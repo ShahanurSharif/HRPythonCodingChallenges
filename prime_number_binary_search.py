7a1a2e07b517a91b40c56fd76d07a93497a5fea4
sharif <shahanur700@gmail.com>
2024-11-12 20:53:45 +0800

    magic

--- prime_number_binary_search.py
+++ prime_number_binary_search.py
@@ -1,6 +1,6 @@
 if __name__ == "__main__":
     low = 1
     high = 311
-    numbers = [i for i in range(low+1, high+1) if i%2!=0]
-    numbers.insert(0,1)
-    print(numbers)
+    for i in range(low, high+1):
+        pass
+
