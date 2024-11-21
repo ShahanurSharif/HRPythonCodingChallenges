48cfb7073339a34d6b4e1b58f6cd3f70c2f9ebf1
sharif <shahanur700@gmail.com>
2024-11-21 11:05:51 +0800

    prime numbers

---
+++ Find_Digits.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    total = 0
+    number_of_divisors = str(n)
+    print(number_of_divisors)
+    for i in number_of_divisors:
+        value = int(i)
+        if value!=0 and n%value==0:
+            total+=1
+    return total
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
