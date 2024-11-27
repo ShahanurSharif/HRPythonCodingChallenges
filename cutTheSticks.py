2808201800f28c077df2f49b50ae3aa3a3db9d28
sharif <shahanur700@gmail.com>
2024-11-27 16:40:37 +0800

    libraryFine

---
+++ cutTheSticks.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'cutTheSticks' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def cutTheSticks(arr):
+    # Write your code here
+    print(arr)
+
+if __name__ == '__main__':
+
+    arr = [5, 4, 4, 2, 2, 8]
+
+    result = cutTheSticks(arr)
+
+    print(result)
---
+++ invoice_generator.py
@@ -0,0 +1,17 @@
+def generate_invoice(arr):
+    pass
+
+if __name__ == '__main__':
+    salary = 0
+    total = 0
+    employee_id = '1001'
+    employee_name = 'Shahanur Md Sharif'
+    Designation = "Founder & Lead Full Stack Developer"
+    Department = "Web & Software Development"
+    gross_salary = "<input>"
+    total_working_days = 0
+    weekends = ["Friday", "Saturday"]
+    holidays = ["Saturday", "Sunday"]
+    lop_days = 0
+    paid_days = total_working_days - len(holidays) - lop_days
+    leaves = 0
