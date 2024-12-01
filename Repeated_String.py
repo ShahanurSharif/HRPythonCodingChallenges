ab98c285b6678cd15e9b5e00a1dc7bb555ba2444
sharif <shahanur700@gmail.com>
2024-12-01 22:43:28 +0800

    repeated string

---
+++ Repeated_String.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    # Write your code here
+    len_s = len(s)
+    count_a = s.count('a')
+    total_number = n/len_s * count_a
+    return math.ceil(total_number)
+
+if __name__ == '__main__':
+
+    s = "bab"
+
+    n = 725261545450
+
+    result = repeatedString(s, n)
+
+    print(result)
