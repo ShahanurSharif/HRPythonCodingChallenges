018cb0d28034d8eec2809cf50582f8c205c74886
sharif <shahanur700@gmail.com>
2024-12-03 11:52:39 +0800

    repeated string

--- equalizeArray.py
+++ equalizeArray.py
@@ -1,5 +1,5 @@
 def equalizeArray(arr):
-    print(sorted(arr))
+    # print(sorted(arr))
     duplicates = []
     seen = set()
     for x in arr:
@@ -8,17 +8,20 @@ def equalizeArray(arr):
         seen.add(x)
 
     len_arr = len(arr)
-    count = 0
-    print(sorted(duplicates))
-    print(len(arr))
+    count = 1
+    # print(sorted(duplicates))
+    # print(len(arr))
     for x in duplicates:
-        count = len_arr - arr.count(x)
+        if count<arr.count(x):
+            count = arr.count(x)
 
-    return count
+
+    return len_arr - count
 
 if __name__ == '__main__':
-    str = "36 12 60 99 78 33 4 21 22 9 12 21 34 76 21 3 3 37 65 27 21 42 11 14 21 88 46 63 79 6 37 94 99 68 76 6 21 86 49 56 22 90 74 83 20 21 94 60 76 75 96 99 92 65 77 26 51 21 77 22 97 34 56"
+    str = "37 32 97 35 76 62"
+
     arr = list(map(int, str.split()))
-    # 55
+    # 5
     result = equalizeArray(arr)
     print(result)
