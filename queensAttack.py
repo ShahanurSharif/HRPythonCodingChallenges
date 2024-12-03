92f005f882f7d84b0432bf802261dc3258d6b744
sharif <shahanur700@gmail.com>
2024-12-03 22:26:40 +0800

    repeated string

--- queensAttack.py
+++ queensAttack.py
@@ -20,7 +20,7 @@ import sys
 #
 
 def createRows(n, queen, obstacle):
-    number_of_row = []
+    number_of_rows = []
     for i in obstacle:
         if queen[0] == i[0]:
             if queen[1] > i[1]:
@@ -29,30 +29,32 @@ def createRows(n, queen, obstacle):
                 number_of_rows = [[queen[0], j] for j in range(1, i[1]) if j != queen[1]]
             else:
                 number_of_rows = [[queen[0], j] for j in range(1, n + 1) if j != queen[1]]
-    return number_of_row
+    return number_of_rows
 
 
 def createColumns(n, queen, obstacle):
-    number_of_column = []
+    number_of_columns = []
     for i in obstacle:
         if queen[1] == i[1]:
             if queen[0] > i[0]:
                 number_of_columns = [[j, queen[1]] for j in range(i[1], n+1) if j!=queen[1]]
             elif queen[0] < i[0]:
                 number_of_columns = [[j, queen[1]] for j in range(queen[1], n+1) if j!=queen[1]]
-    ## q: 2, 3 obs: 4, 3
+            else:
+                number_of_columns = [[j, queen[1]] for j in range(1, n + 1) if j != queen[1]]
+    return number_of_columns
 
-
-def createDialonal(n, queen, obstacle):
-    pass
+# def createDialonal(n, queen, obstacle):
+    # for i in obstacle:
+        # if queen[0] == i[0]:
 
 
 def queensAttack(n, k, r_q, c_q, obstacles):
     number_of_rows = createRows(n, [r_q, c_q], obstacles)
     number_of_columns = createColumns(n, [r_q, c_q], obstacles)
-    number_of_diagonals = createDialonal(n, [r_q, c_q], obstacles)
+    # number_of_diagonals = createDialonal(n, [r_q, c_q], obstacles)
 
-    return len(number_of_rows) + len(number_of_columns) + len(number_of_diagonals)
+    return len(number_of_rows) + len(number_of_columns)
 
 
 if __name__ == '__main__':
