ecde19b8c20db688b8c1bd5fef812c74b33b0a89
sharif <shahanur700@gmail.com>
2024-12-18 23:48:02 +0800

    sort color

--- sort_color_lc.py
+++ sort_color_lc.py
@@ -7,42 +7,14 @@ class Solution:
         """
         # print(nums)
         length = len(nums)
-        if length > 1:
-            middle = length // 2
-            left_arr = nums[:middle]
-            right_arr = nums[middle:]
-            print(left_arr, right_arr)
-
-            self.sortColors(left_arr)
-            self.sortColors(right_arr)
-
-            i = 0  #left index
-            j = 0  #right index
-            k = 0  # merged index
-            while i < len(left_arr) and j < len(right_arr):
-                if left_arr[i] < right_arr[j]:
-                    nums[k] = left_arr[i]
-                    i += 1
-                else:
-                    nums[k] = right_arr[j]
-                    j += 1
-                k += 1
-            #
-            while i<len(nums):
-                nums[k] = left_arr[i]
-                i += 1
-                k += 1
-            #
-            while j<len(nums):
-                nums[k] = left_arr[j]
-                j += 1
-                k += 1
+        count = [0] * (max(nums) - min(nums) + 1)
+        print(count)
 
 
 solution = Solution()
 
 # Define the nums list
-nums = [0, 2, 4, 1, 0]
+nums = [0, 2, 4, 1, 0, 8, 8, 9, 1000, 4, 5]
 
 # Call the sortColors method with nums
 solution.sortColors(nums)
