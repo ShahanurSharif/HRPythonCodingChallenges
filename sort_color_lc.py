1af829ec3c9bf3ea317be48c80b6fa0309c36c2c
sharif <shahanur700@gmail.com>
2024-12-18 23:19:58 +0800

    sort color

--- sort_color_lc.py
+++ sort_color_lc.py
@@ -5,27 +5,38 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        print(nums)
-        # if len(nums) > 1:
-        #     length = len(nums)
-        #     middle = length // 2
-        #     left_arr = nums[:middle]
-        #     right_arr = nums[middle:]
-        #
-        #     self.sortColors(left_arr)
-        #     self.sortColors(right_arr)
-        #
-        #     i = 0  #left index
-        #     j = 0  #right index
-        #     k = 0  # merged index
-        #     while i < len(left_arr) and j < len(right_arr):
-        #         if left_arr < right_arr:
-        #             nums[k] = left_arr[i]
-        #             i += 1
-        #         else:
-        #             nums[k] = right_arr[j]
-        #
-        #         k += 1
+        # print(nums)
+        length = len(nums)
+        if length > 1:
+            middle = length // 2
+            left_arr = nums[:middle]
+            right_arr = nums[middle:]
+            print(left_arr, right_arr)
+
+            self.sortColors(left_arr)
+            self.sortColors(right_arr)
+
+            i = 0  #left index
+            j = 0  #right index
+            k = 0  # merged index
+            while i < len(left_arr) and j < len(right_arr):
+                if left_arr[i] < right_arr[j]:
+                    nums[k] = left_arr[i]
+                    i += 1
+                else:
+                    nums[k] = right_arr[j]
+                    j += 1
+                k += 1
+            #
+            while i<len(nums):
+                nums[k] = left_arr[i]
+                i += 1
+                k += 1
+            #
+            while j<len(nums):
+                nums[k] = left_arr[j]
+                j += 1
+                k += 1
 
 
 solution = Solution()
