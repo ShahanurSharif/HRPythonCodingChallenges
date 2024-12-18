3b96abc921f827901610c53f05992806c71b4ec0
sharif <shahanur700@gmail.com>
2024-12-18 15:53:53 +0800

    recur

--- merge_sort.py
+++ merge_sort.py
@@ -1,7 +1,23 @@
 def mergeSort(arr):
-    pass
+    length = len(arr)
+    if length>1:
+        middle = length//2
+        left_arr = arr[:middle]
+        right_arr = arr[middle:]
+
+        #recur
+        mergeSort(left_arr)
+        mergeSort(right_arr)
+
+        #merge
+        i=0
+        j=0
+        r=0
+        while i < len(left_arr) and j<len(right_arr):
+            print(i, left_arr)
+
 
 
 if __name__ == '__main__':
     arr = [4, 1, 6, 2, 3, 8, 9]
-    print(mergeSort(arr))
+    print('merge', mergeSort(arr))
