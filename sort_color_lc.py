2ab854f04463a7afc52e4220dbc370370fc10614
sharif <shahanur700@gmail.com>
2024-12-19 22:50:33 +0800

    sort color

--- sort_color_lc.py
+++ sort_color_lc.py
@@ -1,14 +1,32 @@
 from typing import List
+
+
+
+
+
 class Solution:
+
+    def merge_sort(self, left_arr, right_arr):
+        pass
+
+    def sort_color_recursive(self, nums):
+        if len(nums) > 1:
+            mid = len(nums) // 2
+            left_arr = self.sort_color_recursive(nums[:mid])
+            right_arr = self.sort_color_recursive(nums[mid:])
+
+            return self.merge_sort(left_arr, right_arr)
+
+
+
 
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         # print(nums)
-        length = len(nums)
-        count = [0] * (max(nums) - min(nums) + 1)
-        print(count)
+        sorted_nums = self.sort_color_recursive(nums)
+        nums[:] = sorted_nums
 
 
 solution = Solution()
