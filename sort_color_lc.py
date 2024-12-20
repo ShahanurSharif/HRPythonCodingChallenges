bf7e35f9b3c1a06189fc96e430e037f26d7052c0
sharif <shahanur700@gmail.com>
2024-12-20 11:03:01 +0800

    sort function

--- sort_color_lc.py
+++ sort_color_lc.py
@@ -1,42 +1,38 @@
 from typing import List
 
-
-
-
-
 class Solution:
-
     def merge_sort(self, left_arr, right_arr):
-        sorted_arr = []
-        i=j=0
-        print('value', left_arr, len(left_arr), right_arr, len(right_arr))
-        # while i < len(left_arr) and j < len(right_arr):
-        #     if left_arr[i] < right_arr[j]:
-        #         sorted_arr.append(left_arr[i])
-
-        # return [0]
+        pass
 
     def sort_color_recursive(self, nums: List[int]):
         length = len(nums)
-        if length <=1:
+        if length <= 1:
             return nums
-
+        
         mid = length // 2
-        print('recursion', nums)
+
         left_arr = self.sort_color_recursive(nums[:mid])
         right_arr = self.sort_color_recursive(nums[mid:])
-        print('after recursion',left_arr, right_arr)
-        return self.merge_sort(left_arr, right_arr)
-            # return [0]
+
+        sorted_arr = []
+        i=j=0
+        while i<len(left_arr) and j<(right_arr):
+            if left_arr[i] < right_arr[j]:
+                sorted_arr.append(left_arr[i])
+                i+=1
+            else:
+                sorted_arr.append(right_arr[j])
+                j+=1
+
+        print(left_arr[i:], right_arr[j:])
 
 
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # print(nums)
-        self.sort_color_recursive(nums)
-
+        sorted_nums = self.sort_color_recursive(nums)
+        nums[:] = sorted_nums
 
 solution = Solution()
 
@@ -45,4 +41,4 @@ nums = [0, 2, 4, 1, 0, 8, 8, 9, 1000, 4, 5, 0]
 
 # Call the sortColors method with nums
 solution.sortColors(nums)
-# print(nums)
+print(nums)
