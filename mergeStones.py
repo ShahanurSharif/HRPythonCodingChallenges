b2d5dc19e40836ff068435e0ffb4608a51bde291
sharif <shahanur700@gmail.com>
2024-12-20 16:31:50 +0800

    merge stone

--- mergeStones.py
+++ mergeStones.py
@@ -5,18 +5,25 @@ from django.template.defaultfilters import length
 
 class Solution:
     def mergeStones(self, stones: List[int], k: int) -> int:
-        length = len(stones)
-        mid = length // 2
-        lowest = 0
-        for i in range(len(stones)-1):
-            chunk_value = stones[i:i+k]
-            sum_chunk = sum(chunk_value)
-            sum_chunk_next = sum(stones[i+1:i+1+k])
-            if sum_chunk < sum_chunk_next:
+        lowest = sum(stones[:k])
+        lowest_index = 0
+        # list the consecutive smallest sum
+        for i in range(k, len(stones)):
+            start = i - 1
+            end = i - 1 + k
+            # print(i-1, k+i, stones[start:end])
+            sum_chunk = sum(stones[start:end])
+            if sum_chunk < lowest:
                 lowest = sum_chunk
+                lowest_index = start
 
-            else:
-                lowest = sum_chunk_next
+        # print(stones)
+        if len(stones)<=1:
+            return stones[0]
+        else:
+            stones[lowest_index] = lowest
+            stones.pop(lowest_index-1+k)
+            self.mergeStones(stones, k)
 
 
 
@@ -24,3 +31,4 @@ mergeStone = Solution()
 stones = [3, 2, 4, 1]
 k = 2
 value = mergeStone.mergeStones(stones, k)
+print(value)
