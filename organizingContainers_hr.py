a69df235a6dea676c3273869ab2c64a299461c23
sharif <shahanur700@gmail.com>
2024-12-21 11:38:29 +0800

    merge stone

---
+++ organizingContainers_hr.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'organizingContainers' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts 2D_INTEGER_ARRAY container as parameter.
+#
+
+def organizingContainers(container):
+    pass
+    # Write your code here
+
+
+if __name__ == '__main__':
+
+    container =[[1, 1], [1, 1]]
+
+    result = organizingContainers(container)
+    print(result)
