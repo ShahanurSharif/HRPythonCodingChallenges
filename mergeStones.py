60fcb7ba46e81bfd064e00991ff10679494d7072
sharif <shahanur700@gmail.com>
2024-12-21 13:57:00 +0800

    initialize the total

--- mergeStones.py
+++ mergeStones.py
@@ -2,11 +2,8 @@ from typing import List
 
 from django.template.defaultfilters import length
 
-total = 0
-
 class Solution:
-    def mergeStones(self, stones: List[int], k: int) -> int:
-        global total
+    def mergeStones(self, stones: List[int], k: int, total=0) -> int:
         lowest = sum(stones[:k])
         lowest_index = 0
         for i in range(1, len(stones) - 1 ):
@@ -19,10 +16,7 @@ class Solution:
             if sum_chunk < lowest:
                 lowest = sum_chunk
                 lowest_index = start
-                # total = lowest
-        #         # print('lowest',lowest)
-        #
-        #
+
         if len(stones)==1:
             # print('hello 1', total, stones, stones[0])
             return total
@@ -30,9 +24,10 @@ class Solution:
             stones[lowest_index] = lowest
             del stones[lowest_index+1:lowest_index+k]
             total += lowest
-            return self.mergeStones(stones, k)
+            return self.mergeStones(stones, k, total)
 
 
-stones = [3, 5, 1, 2, 6]
-value = Solution().mergeStones(stones, 3)
-print('final', value, stones)
+stones = [3,5,1,2,6]
+k=3
+value = Solution().mergeStones(stones, k)
+print('final', value)
