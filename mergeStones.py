c62b43e8d9a3297bacca146dc1f137eb2e923251
sharif <shahanur700@gmail.com>
2024-12-21 23:34:53 +0800

    initialize the total

--- mergeStones.py
+++ mergeStones.py
@@ -4,7 +4,11 @@ from django.template.defaultfilters import length
 
 class Solution:
     def mergeStones(self, stones: List[int], k: int, total=0) -> int:
-        lowest = sum(stones[:k])
+        str_order_sum = sum(stones[:k])
+        reverse_order = stones[len(stones)-k+1:len(stones)]
+        reverse_order.append(stones[0])
+        reverse_order_sum = sum(reverse_order)
+
         lowest_index = 0
         for i in range(1, len(stones) - 1 ):
             start = i
@@ -12,6 +16,9 @@ class Solution:
             if len(stones) - end < 0:
                 break
 
+            straigth_sum = sum(stones[start:end])
+            print('hello', stones[start::-(i+k)])
+            # start_to_end =
             sum_chunk = sum(stones[start:end])
             if sum_chunk < lowest:
                 lowest = sum_chunk
@@ -27,7 +34,8 @@ class Solution:
             return self.mergeStones(stones, k, total)
 
 
-stones = [3,5,1,2,6]
-k=3
-value = Solution().mergeStones(stones, k)
-print('final', value)
+stones = [6,4,4,6,1]
+print(stones[len(stones)-3+1:len(stones)])
+# k=2
+# value = Solution().mergeStones(stones, k)
+# print('final', value)
