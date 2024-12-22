61e9399ddb2e404ef5946d6fb4c1a7dd6592c19b
sharif <shahanur700@gmail.com>
2024-12-22 20:17:49 +0800

    initialize the total

--- bfs_basic.py
+++ bfs_basic.py
@@ -1,9 +1,15 @@
-
-def bfs(edges, v):
+def bfs(edges, param):
     pass
 
-edges = [(0, 1), (0, 2), (1, 3), (2, 3)]
 
-print(bfs(edges, 0))
-
-# print(bfs(edges, 0))
+if __name__ == "__main__":
+    edges = {
+        0:[1, 2, 3],
+        1:[2, 5, 6],
+        2:[3, 5, 6],
+        3:[6, 7, 8]
+    }
+    a=[12, 3, 4]
+    while a:
+        print(a)
+    # bfs(edges, 0)
