a343ddf63c49d9d453decae4ae41107278af00ed
sharif <shahanur700@gmail.com>
2024-12-22 21:04:35 +0800

    initialize the total

--- bfs_basic.py
+++ bfs_basic.py
@@ -1,15 +1,26 @@
-def bfs(edges, param):
-    pass
+edges = {
+    0: [1, 2, 3],
+    1: [2, 5, 6],
+    2: [3, 5, 6],
+    3: [6, 7, 8]
+}
+
+visited = []
+queue = []
+
+def bfs(edges, root):
+    visited.append(root)
+    queue.append(root)
+
+    while queue:
+        m = queue.pop(0)
+        # print(m, end = " ")
+        for neighbor in edges[m]:
+            print('edges', neighbor, [m])
+            # if neighbor not in visited:
+
+
 
 
 if __name__ == "__main__":
-    edges = {
-        0:[1, 2, 3],
-        1:[2, 5, 6],
-        2:[3, 5, 6],
-        3:[6, 7, 8]
-    }
-    a=[12, 3, 4]
-    while a:
-        print(a)
-    # bfs(edges, 0)
+    bfs(edges, 0)
