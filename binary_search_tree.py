cd08b7961d06ec7fff7a4af0a307eefcab3105e5
sharif <shahanur700@gmail.com>
2024-12-24 01:12:47 +0800

    initialize the total

---
+++ binary_search_tree.py
@@ -0,0 +1,51 @@
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        # print('add_child', data, self.data)
+        if data == self.data:
+            return
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+            # print('from left', elements)
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+            # print('from right', elements)
+
+        return elements
+
+
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+    print('from root',root)
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+
+
+if __name__ == '__main__':
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+    numbers_tree = build_tree(numbers)
+    print(numbers_tree.in_order_traversal())
