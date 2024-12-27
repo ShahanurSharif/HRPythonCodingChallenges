8d1bbee384715a22a44d4e553fe849c892c70366
sharif <shahanur700@gmail.com>
2024-12-28 00:24:36 +0800

    initialize the total

--- organizingContainers_hr.py
+++ organizingContainers_hr.py
@@ -14,8 +14,21 @@ import sys
 #
 
 def organizingContainers(container):
+    # 1 3 1
+    #-> 0 1 1 1 2
+    # 2 1 2
+    #-> 2 2 1 0 0
+    # 3 3 3
+    #-> 0 0 0 1 1 1 2 2 2
+
+    #0 2 1
+    #-> 0 1 1 2
+    #1 1 1
+    #-> 2 1 0 2
+    #2 0 0
+    #-> 0 0
+
     pass
-    # Write your code here
 
 
 if __name__ == '__main__':
