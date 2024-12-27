7eef2956e33f0f721315f2d28de365c881b69a50
sharif <shahanur700@gmail.com>
2024-12-27 20:53:18 +0800

    initialize the total

--- Binary_Search_tree_Geek.py
+++ Binary_Search_tree_Geek.py
@@ -17,24 +17,37 @@ class BSTNode:
         if value < self.value:
             if self.left:
                 self.left.insert(value)
-            else:
-                self.left = BSTNode(value)
+                return
+
+            self.left = BSTNode(value)
+            return
 
         if value > self.value:
             if self.right:
                 self.right.insert(value)
-            else:
-                self.right = BSTNode(value)
+                return
+            self.right = BSTNode(value)
+            return
 
-        print(self.value)
+
+    def preorder(self, vals):
+        if self.val is not None:
+            vals.append(self.val)
+        if self.left is not None:
+            self.left.preorder(vals)
+        if self.right is not None:
+            self.right.preorder(vals)
+        return vals
 
 
 
 if "__main__" == __name__:
-    # nums = [12, 6, 18, 19, 21, 11, 3, 5, 4, 24, 18]
-    nums = [12, 6, 18]
+    nums = [12, 6, 18, 19, 21, 11, 3, 5, 4, 24, 18]
+    # nums = [12, 6, 18]
     bst = BSTNode()
     for num in nums:
         bst.insert(num)
+
+    print(bst.preorder([]))
 
 
