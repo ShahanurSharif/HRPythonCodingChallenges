b6d92862c147008c6a5393c29c43a46ecd0e9bb8
sharif <shahanur700@gmail.com>
2024-12-28 20:08:16 +0800

    initialize the total

--- organizingContainers_hr.py
+++ organizingContainers_hr.py
@@ -7,6 +7,7 @@ import re
 import sys
 
 from PIL.ImageOps import contain
+from numpy.lib.user_array import container
 
 
 #
@@ -17,17 +18,25 @@ from PIL.ImageOps import contain
 #
 
 def organizingContainers(container):
-    length = len(container)
-    total = list(map(sum, zip(*container)))
-    print(total)
+    column_total = sorted(list(map(sum, zip(*container))))
+    row_total = sorted(list(map(sum, container)))
+    print(row_total, column_total)
+    # print(column_total)
+    if column_total != row_total:
+        return 'Impossible'
 
+    return "Possible"
 
 
 
 if __name__ == '__main__':
 
-    # container =[[1, 3, 1], [2, 1, 2], [3, 3, 3]]
-    container =[[0, 2, 1], [1, 1, 1], [2, 0, 0]]
+    container =[
+        [1, 3, 1],
+        [2, 1, 2],
+        [3, 3, 3]
+    ]
+    # container =[[0, 2, 1], [1, 1, 1], [2, 0, 0]]
 
     result = organizingContainers(container)
     print(result)
