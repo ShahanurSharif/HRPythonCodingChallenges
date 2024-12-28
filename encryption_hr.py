6018f590064940daba9f848a5311f2227b32925b
sharif <shahanur700@gmail.com>
2024-12-28 22:31:49 +0800

    initialize the total

--- encryption_hr.py
+++ encryption_hr.py
@@ -20,20 +20,18 @@ def encryption(s):
     x_row = math.floor(x_root)
     x_col = math.ceil(x_root)
     new_string = ""
+    k=0
     for i in range(0, length):
         if i<x_col:
             for j in range(0, length):
                 if (j+x_col-i)%x_col==0:
+                    if k!=i:
+                        new_string += ' '+s[j]
+                    else:
+                        new_string += s[j]
+                    k=i
 
-                    new_string += s[j]
-        #
-        # if (i+1)%4==0:
-        #    new_string += s[i]+' '
-        # else:
-        #     new_string += s[i]
-        #
-
-    print(new_string)
+    return new_string
 
 
 if __name__ == '__main__':
