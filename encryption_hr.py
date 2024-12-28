ebdb52d33457d2404d7ef7ff0a05c3d83c01b6ee
sharif <shahanur700@gmail.com>
2024-12-28 21:22:56 +0800

    initialize the total

---
+++ encryption_hr.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'encryption' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def encryption(s):
+    # Write your code here
+    length = len(s)
+    x_root = math.sqrt(length)
+    x_row = math.floor(x_root)
+    x_col = math.ceil(x_root)
+    new_string = ""
+    for i in range(0, length):
+        for j in range(0, length):
+            pass
+        #
+        # if (i+1)%4==0:
+        #    new_string += s[i]+' '
+        # else:
+        #     new_string += s[i]
+        #
+
+    print(new_string)
+
+
+if __name__ == '__main__':
+    s = "haveaniceday"
+    result = encryption(s)
+    print(result)
