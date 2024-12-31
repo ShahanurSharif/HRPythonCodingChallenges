c87697605cda9b89b6afe1493a1b9ed45c0a6127
sharif <shahanur700@gmail.com>
2024-12-31 14:24:06 +0800

    initialize the total

--- dfs.py
+++ dfs.py
@@ -5,5 +5,13 @@ def visit(node):
     pass
 
 
+def neighbors(node):
+    pass
+
+
 def Dfs(arr, node):
     visit(node)
+    marked[node] = True
+    for i in neighbors(node):
+        if not marked[i]:
+            Dfs(arr, i)
