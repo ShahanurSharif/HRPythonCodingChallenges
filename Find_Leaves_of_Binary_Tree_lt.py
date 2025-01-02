5fdcd0e261f4349e50c5989f8b11a5df04d1e279
sharif <shahanur700@gmail.com>
2025-01-02 18:34:58 +0800

    initialize the total

--- Find_Leaves_of_Binary_Tree_lt.py
+++ Find_Leaves_of_Binary_Tree_lt.py
@@ -13,19 +13,33 @@ class Solution:
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
 
         def dfs(node: Optional[TreeNode], leaves: List[List[int]]) -> int:
-            if not node: return -1
+            print(node, leaves)
+            if not node:
+                return -1
+
+            left_height = dfs(node.left, leaves)
+            right_height = dfs(node.right, leaves)
+            print(left_height, right_height, leaves)
+
+            curr_height = max(left_height, right_height) + 1
+
+            if curr_height == len(leaves):
+                leaves.append([])
+
+            leaves[curr_height].append(node.val)
+            return curr_height
 
 
         result = []
         dfs(root, result)
         return result
 
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
 
-root = TreeNode(1)
-root.left = TreeNode(2)
-root.right = TreeNode(3)
-root.left.left = TreeNode(4)
-root.left.right = TreeNode(5)
-
-value = Solution()
-print(value.findLeaves(root))
+    value = Solution()
+    print(value.findLeaves(root))
