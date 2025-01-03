ca748be8866e76edc6da48a6d530e560a506d2f2
sharif <shahanur700@gmail.com>
2025-01-03 22:30:57 +0800

    initialize the total

--- Same_Tree_lt.py
+++ Same_Tree_lt.py
@@ -1,4 +1,4 @@
-import json
+# Definition for a binary tree node.
 from typing import Optional
 
 
@@ -12,16 +12,24 @@ class TreeNode:
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         print(p, q)
-        # if len(p) != len(q):
-        #     return False
+        if len(p) != len(q):
+            return False
+        def level_order_traversal(root: Optional[TreeNode]) -> Optional[list]:
+            if root is None:
+                return []
 
 
-        return True
 
 
-if __name__ == '__main__':
-    solution = Solution()
-    value = (solution.isSameTree(
-        TreeNode(1, TreeNode(2), TreeNode(3)), TreeNode(4)),
-        TreeNode(1, TreeNode(2), TreeNode(3)), TreeNode(4)
-    )
+
+if __name__ == "__main__":
+    p = TreeNode(1)
+    p.left = TreeNode(2)
+    p.right = TreeNode(3)
+
+    q = TreeNode(1)
+    q.left = TreeNode(2)
+    q.right = TreeNode(3)
+
+    sol = Solution()
+    print(sol.isSameTree(p, q))
