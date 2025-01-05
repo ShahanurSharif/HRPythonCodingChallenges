651001bc1aff03dce0c09f6308bdfa63ffa297b6
sharif <shahanur700@gmail.com>
2025-01-05 20:41:28 +0800

    initialize the total

--- averageOfSubtree_lt.py
+++ averageOfSubtree_lt.py
@@ -7,4 +7,18 @@ class TreeNode:
 
 class Solution:
     def averageOfSubtree(self, root: TreeNode) -> int:
-        pass
+        avg = 0
+        return avg
+
+
+if __name__ == '__main__':
+    root = TreeNode(4)
+    root.left = TreeNode(8)
+    root.left.left = TreeNode(0)
+    root.left.right = TreeNode(1)
+    root.right = TreeNode(5)
+    root.right.right = TreeNode(6)
+
+    solution = Solution()
+    print(solution.averageOfSubtree(root))
+
