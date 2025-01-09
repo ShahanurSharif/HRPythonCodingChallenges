2377729378271f42b36fe772bd05ae97b155f9fb
sharif <shahanur700@gmail.com>
2025-01-10 00:41:12 +0800

    is symmetric

--- leetcode/isSymmetric.py
+++ leetcode/isSymmetric.py
@@ -9,27 +9,30 @@ class TreeNode:
 
 
 class Solution:
-    def is_validate(self, root, total=[]):
+    def is_validate(self, root, total=[], left_val=[], right_val=[]) -> bool:
         if not root:
             return True
 
         queue = [root]
+
         while queue:
             node = queue.pop(0)
             total.append(node.val)
 
             if node.left:
                 queue.append(node.left)
+                left_val.append(node.left.val)
                 '''
                 2
                 '''
 
             if node.right:
                 queue.append(node.right)
+                right_val.append(node.right.val)
                 '''
                 2
                 '''
-        print(total)
+        print(total, left_val, right_val)
         return False
 
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
@@ -38,12 +41,25 @@ class Solution:
 
 if __name__ == '__main__':
     q = TreeNode(2)
+
     q.left = TreeNode(3)
     q.right = TreeNode(3)
+
     q.left.left = TreeNode(4)
     q.left.right = TreeNode(5)
     q.right.left = TreeNode(5)
     q.right.right = TreeNode(4)
+
+    q.left.left.left = TreeNode(6)
+    q.left.left.right = TreeNode(7)
+    q.left.right.left = TreeNode(8)
+    q.left.right.right = TreeNode(9)
+
+    q.right.left.left = TreeNode(9)
+    q.right.left.right = TreeNode(8)
+    q.right.right.left = TreeNode(7)
+    q.right.right.right = TreeNode(6)
+
     '''
                  2
             /        \
