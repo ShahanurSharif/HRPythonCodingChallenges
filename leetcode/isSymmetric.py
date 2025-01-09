1ef82b85cfd323a3ff710caffe63825cdea2c142
sharif <shahanur700@gmail.com>
2025-01-09 20:48:27 +0800

    is symmetric

--- leetcode/isSymmetric.py
+++ leetcode/isSymmetric.py
@@ -8,18 +8,35 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def bfs(self, root, left, right):
-        pass
+    def is_validate(self, root):
+        if not root:
+            return True
+
+        queue = [root]
+        while queue:
+            node = queue.pop(0)
+
+            if node.left:
+                queue.append(node.left)
+                '''
+                2
+                '''
+
+            if node.right:
+                queue.append(node.right)
+                '''
+                2
+                '''
 
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        left = float('-inf')
-        right = float('inf')
-        return self.bfs(root, left, right)
+        return self.is_validate(root)
 
 if __name__ == '__main__':
     q = TreeNode(1)
     q.left = TreeNode(2)
     q.right = TreeNode(2)
+    q.left.right = TreeNode(3)
+    q.right.left = TreeNode(3)
 
     solution = Solution()
     print(solution.isSymmetric(q))
