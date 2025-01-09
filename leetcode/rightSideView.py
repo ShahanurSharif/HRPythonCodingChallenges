fa9d331d6b1fd5cb614c37968409dd5d2eb40cec
sharif <shahanur700@gmail.com>
2025-01-09 15:49:14 +0800

    initialize the total

--- leetcode/rightSideView.py
+++ leetcode/rightSideView.py
@@ -8,23 +8,41 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def dfs(self, root, arr=[]):
+    def dfs(self, root, level=0, arr=[]):
         if root is None:
             return
+        if level == len(arr):
+            arr.append(root.val)
 
-        arr.append(root.val)
-        self.dfs(root.right, arr)
+        self.dfs(root.right, level+1, arr)
+        self.dfs(root.left, level+1, arr)
         return arr
 
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        return self.dfs(root)
+        result = []
+        return self.dfs(root, 0, result)
 
 
 if __name__ == '__main__':
     p = TreeNode(1)
     p.left = TreeNode(2)
+    p.left.left = TreeNode(4)
+    p.left.left.left = TreeNode(5)
     p.right = TreeNode(3)
+
+    sol = Solution()
+    print(sol.rightSideView(p))
+
+
+    p = TreeNode(1)
+    # p.left = TreeNode(2)
+    # p.left.left = TreeNode(4)
+    # p.left.left.left = TreeNode(5)
+    p.right = TreeNode(3)
+
+
 
 
     sol = Solution()
     print(sol.rightSideView(p))
+
