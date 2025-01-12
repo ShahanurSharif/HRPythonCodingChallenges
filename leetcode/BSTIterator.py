26cadf4dcedd19108798038f0865cea1765aecf1
sharif <shahanur700@gmail.com>
2025-01-12 20:55:22 +0800

    is symmetric

---
+++ leetcode/BSTIterator.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        pass
+
+    def next(self) -> int:
+        pass
+
+    def hasNext(self) -> bool:
+        pass
+
+# Your BSTIterator object will be instantiated and called as such:
+if __name__ == '__main__':
+    root = TreeNode(5)
+
+    obj = BSTIterator(root)
+    param_1 = obj.next()
+    param_2 = obj.hasNext()
