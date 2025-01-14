ed414edf7334d864e299858b74445cfd10690298
sharif <shahanur700@gmail.com>
2025-01-14 15:27:45 +0800

    is symmetric

--- leetcode/BSTIterator.py
+++ leetcode/BSTIterator.py
@@ -18,6 +18,24 @@ the smallest element in the BST.
 You may assume that next() calls will always be valid.
 That is, there will be at least a next number
 in the in-order traversal when next() is called.
+
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
 '''
 
 from typing import Optional
@@ -31,7 +49,13 @@ class TreeNode:
 
 class BSTIterator:
     def __init__(self, root: Optional[TreeNode]):
-        print(root)
+        self.stack = []
+        self._leftmost_inorder(root)
+
+    def _leftmost_inorder(self, root):
+        if not root: return None
+        while root:
+            pass
 
     def next(self) -> int:
         pass
