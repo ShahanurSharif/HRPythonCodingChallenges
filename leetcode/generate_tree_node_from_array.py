dcd39dee8ea00a8a2ceded2e2d79c19fa0a1341a
sharif <shahanur700@gmail.com>
2025-01-15 23:05:48 +0800

    is symmetric

--- leetcode/generate_tree_node_from_array.py
+++ leetcode/generate_tree_node_from_array.py
@@ -9,8 +9,32 @@ class TreeNode:
 
 
 def generate_tree(arr: List[Optional[int]])-> Optional[TreeNode]:
+    if not arr:
+        return None
 
-    return TreeNode(arr[0])
+    root = TreeNode(arr[0])
+    queue = [root]
+    i=0
+    while i < len(arr):
+        current = queue.pop(0)
+        '''
+        left = 2 * i + 1 
+        right = 2 * i + 2
+        '''
+
+        if i<len(arr) and arr[i] is not None:
+            current.left = TreeNode(arr[i])
+            queue.append(current.left)
+
+        i += 1
+
+        if i<len(arr) and arr[i] is not None:
+            current.right = TreeNode(arr[i])
+            queue.append(current.right)
+
+        i += 1
+
+    return root
 
 
 if __name__ == '__main__':
