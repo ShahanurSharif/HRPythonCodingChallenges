bbd4766194f34966d5d6103911f0df0c2e4a3a5f
sharif <shahanur700@gmail.com>
2025-01-16 10:21:09 +0800

    is symmetric

--- leetcode/insertIntoBST.py
+++ leetcode/insertIntoBST.py
@@ -1,3 +1,37 @@
+'''
+https://leetcode.com/problems/insert-into-a-binary-search-tree/description/
+
+You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3], val = 5
+Output: [4,2,7,1,3,5]
+Explanation: Another accepted tree is:
+
+Example 2:
+
+Input: root = [40,20,60,10,30,50,70], val = 25
+Output: [40,20,60,10,30,50,70,null,null,25]
+Example 3:
+
+Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+Output: [4,2,7,1,3,5]
+
+
+Constraints:
+
+The number of nodes in the tree will be in the range [0, 104].
+-108 <= Node.val <= 108
+All the values Node.val are unique.
+-108 <= val <= 108
+It's guaranteed that val does not exist in the original BST.
+'''
 # Definition for a binary tree node.
 from typing import Optional
 
@@ -12,11 +46,16 @@ class Solution:
         if root is None:
             return TreeNode(val)
 
-        left_value = self.insertIntoBST(root.left, val)
-        right_value = self.insertIntoBST(root.right, val)
+        print(TreeNode, root)
 
-        if val < left_value.val or val > right_value.val:
+        if root.left is not None and val<root.left.val:
             return TreeNode(val)
+
+        if root.right is not None and val < root.right.val:
+            return TreeNode(val)
+
+        self.insertIntoBST(root.left, val)
+        self.insertIntoBST(root.right, val)
 
 
 
