30cca04ee363acc63ddc8b9e1a8b698a9b717c3a
sharif <shahanur700@gmail.com>
2025-01-23 15:04:39 +0800

    sellingWood

--- leetcode/lengthOfLongestSubstring.py
+++ leetcode/lengthOfLongestSubstring.py
@@ -43,34 +43,7 @@ from django.template.defaultfilters import length
 #   0 1 2 3 4 5 6 7
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 0:  # If the string is empty, return 0
-            return 0
-        if len(s) == 1:  # If the string has one character, return 1
-            return 1
-
-        count = 0
-        i = 0
-        target_value = s[0]
-        remaining_value = s
-        length_remaining_value = len(remaining_value)
-
-        while i < length_remaining_value:
-            # Check if the current target character appears again in the remaining string
-            if target_value in remaining_value[i + 1:]:
-                location = remaining_value[i + 1:].index(target_value) + (i + 1)
-
-                # Update the count for the length of the current substring
-                count = max(count, location - i)
-
-                # Move the target value to the next character
-                i += 1
-                target_value = remaining_value[i]
-            else:
-                # No duplicate found; count the rest of the string
-                count = max(count, length_remaining_value - i)
-                break
-
-        return count
+        pass
 
 
 if __name__ == '__main__':
