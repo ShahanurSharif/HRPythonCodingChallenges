2116cf1bd4280a85d249a05b3505875f29a629e5
sharif <shahanur700@gmail.com>
2025-01-30 10:25:02 +0800

    sellingWood

---
+++ basic/binary_search.py
@@ -0,0 +1,28 @@
+'''
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+'''
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = nums[0]
+        high = len(nums) - 1
+        mid = len(nums) // 2
+
+        while high < low:
+            if nums[mid] == target:
+                return nums.index(target)
+
+            if nums[mid] < target:
+                low = nums[mid + 1]
+            else:
+                high = nums[mid - 1]
+
+            mid = mid // 2
