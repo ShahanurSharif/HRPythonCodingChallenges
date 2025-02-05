86f8a0a876cd15a2460e773a55e3dd0e2aeff828
sharif <shahanur700@gmail.com>
2025-02-05 22:27:10 +0800

    findPeakElement

--- basic/binarySearch/findPeakElement.py
+++ basic/binarySearch/findPeakElement.py
@@ -41,7 +41,10 @@ class Solution:
         low, high = 0, len(nums) - 1
         while low <= high:
             mid = (low + high) // 2
-
+            if nums[mid] > nums[mid + 1]:
+                high = mid
+            else:
+                low = mid + 1
 
         return high
 
