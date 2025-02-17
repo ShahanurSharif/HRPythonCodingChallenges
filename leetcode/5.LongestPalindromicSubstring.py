aadc08f130ef54926e6d7eb38ffd992705a73990
sharif <shahanur700@gmail.com>
2025-02-17 21:55:25 +0800

    longestPalindrome

--- leetcode/5.LongestPalindromicSubstring.py
+++ leetcode/5.LongestPalindromicSubstring.py
@@ -46,9 +46,7 @@ class Solution:
     def is_palindrome(self, st: str) -> bool:
         return st == st[::-1]
 
-
     def make_palindrome(self, s, right, val):
-
         new_str = s[val[s[right]][0]: right + 1]
         if self.is_palindrome(new_str):
             val[s[right]].append(right)
@@ -57,13 +55,13 @@ class Solution:
             for i in range(len(val[s[right]])):
                 if s[right] == s[val[s[right]][i]]:
                     if self.is_palindrome(s[val[s[right]][i]: right + 1]):
-                        if len(val[s[right]]) < len(s[val[s[right]][i]: right + 1]):
+                        total_len = val[s[right]][-1] - val[s[right]][0]
+                        if total_len < len(s[val[s[right]][i]: right + 1]):
                             remove_indexes.append(i)
                             val[s[right]].append(right)
-            # print(remove_indexes[-1])
             if remove_indexes:
                 val[s[right]] = val[s[right]][remove_indexes[-1]:]
-            print(val, new_str)
+
         return val
 
     def longestPalindrome(self, s: str) -> str:
@@ -72,54 +70,55 @@ class Solution:
             return s
         value = dict()
 
-        highest_value = [0, 1, 0]
+        highest_value = [0, 0, 0]
         for i in range(len(s)):
             if s[i] in value:
                 value = self.make_palindrome(s, i, value)
             else:
                 value[s[i]]=[i]
-
-            if len(value[s[i]]) > highest_value[-1]:
-                highest_value = [value[s[i]][0], value[s[i]][-1], len(value[s[i]])]
-
+            total_len = value[s[i]][-1] - value[s[i]][0]
+            if total_len > highest_value[-1]:
+                highest_value = [value[s[i]][0], value[s[i]][-1], total_len]
+        # print(highest_value)
         return s[highest_value[0]:highest_value[-2]+1]
 
 
 if __name__ == '__main__':
     testcases = [
+        ("abbcccbbbcaaccbababcbcabca", "bbcccbb"),
         # Basic cases
         ("aacabdkacaa", "aca"),  # Provided test case
 
         # Edge cases
-        # ("a", "a"),  # Single character string
-        # ("aa", "aa"),  # Two identical characters
-        # ("ab", "a"),  # Two different characters
+        ("a", "a"),  # Single character string
+        ("aa", "aa"),  # Two identical characters
+        ("ab", "a"),  # Two different characters
         #
         # Palindromes of different lengths
-        # ("racecar", "racecar"),  # Entire string is a palindrome
-        # ("babad", "bab"),  # Multiple valid outputs ("aba" also valid)
-        # ("cbbd", "bb"),  # Even length palindrome
+        ("racecar", "racecar"),  # Entire string is a palindrome
+        ("babad", "bab"),  # Multiple valid outputs ("aba" also valid)
+        ("cbbd", "bb"),  # Even length palindrome
         #
         # # Longest palindrome at edges
-        # ("abcracecar", "racecar"),  # Palindrome at the end
-        # ("racecarxyz", "racecar"),  # Palindrome at the beginning
+        ("abcracecar", "racecar"),  # Palindrome at the end
+        ("racecarxyz", "racecar"),  # Palindrome at the beginning
         #
         # # All identical characters
-        # ("aaaaaa", "aaaaaa"),  # Entire string is palindrome
+        ("aaaaaa", "aaaaaa"),  # Entire string is palindrome
         #
         # # No palindromes longer than 1
-        # ("abcdefg", "a"),  # All unique characters
+        ("abcdefg", "a"),  # All unique characters
         #
         # # Mixed cases
-        # ("abacdfgdcaba", "aba"),  # Multiple small palindromes
-        # ("bananas", "anana"),  # Odd-length palindrome
+        ("abacdfgdcaba", "aba"),  # Multiple small palindromes
+        ("bananas", "anana"),  # Odd-length palindrome
         #
         # # Special characters and digits
-        # ("1234321abc", "1234321"),  # Numeric palindrome
-        # ("a1b2b1a", "a1b2b1a"),  # Mixed characters palindrome
+        ("1234321abc", "1234321"),  # Numeric palindrome
+        ("a1b2b1a", "a1b2b1a"),  # Mixed characters palindrome
         #
         # # Large input case
-        # ("a" * 1000, "a" * 1000),  # Stress test (all identical)
+        ("a" * 1000, "a" * 1000),  # Stress test (all identical)
 
     ]
 
@@ -127,9 +126,5 @@ if __name__ == '__main__':
     for testcase in testcases:
         # pass
         value = solution.longestPalindrome(testcase[0])
-        print(value)
-        try:
-            assert value == testcase[1]
-        except AssertionError as e:
-            print(f'Assertion failed at the {str(e)} line')
-            exit(1)
+        # print(value)
+        assert value == testcase[1]
