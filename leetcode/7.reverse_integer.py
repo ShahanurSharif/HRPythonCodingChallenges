4ab1781143564237a8d00d8f1d3d0e6ecd8300ef
sharif <shahanur700@gmail.com>
2025-03-05 22:43:30 +0800

    reverse integer

--- leetcode/7.reverse_integer.py
+++ leetcode/7.reverse_integer.py
@@ -5,7 +5,7 @@ Topics
 Companies
 Given a signed 32-bit integer x, return x with its digits reversed.
 If reversing x causes the value to go outside the signed 32-bit
-integer range [-231, 231 - 1], then return 0.
+integer range [-2^31, 2^31 - 1], then return 0.
 
 Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
@@ -30,29 +30,32 @@ Constraints:
 -231 <= x <= 231 - 1
 
 '''
+from audioop import reverse
+
+
 class Solution:
     def reverse(self, x: int) -> int:
-        value = 0
-        if x.bit_length()>32:
-            return value
+        if x<0:
+            reversed_value = int(str(x)[::-1])
+        else:
+            reversed_value = int(str(x))
 
-        for i in str(x):
-            print(i)
-
-        return value
+        print(reversed_value)
+        # if reversed_value<-2**31 and reversed_value>2^31 - 1:
+        #     pass
 
 if __name__ == '__main__':
     test_cases = [
-        (123, 321),
-        (-123, -321),
-        (120, 21),
-        (0, 0),
-        (1534236469, 0),   # Overflow case
+        # (123, 321),
+        # (-123, -321),
+        # (120, 21),
+        # (0, 0),
+        # (1534236469, 0),   # Overflow case
         (-2147483648, 0),  # Overflow case
-        (1463847412, 2147483641),
-        (-1463847412, -2147483641),
-        (1000000003, 0),   # Overflow case
-        (-1000000003, 0)   # Overflow case
+        # (1463847412, 2147483641),
+        # (-1463847412, -2147483641),
+        # (1000000003, 0),   # Overflow case
+        # (-1000000003, 0)   # Overflow case
     ]
     s = Solution()
     for case in test_cases:
